1c857eaf89ea6eecafd8bdf4715572cc8d88d1aa
Sukirti-s <sukirti.s.34@gmail.com>
2025-08-25 14:25:52 +0530

    25/08/25 update

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,20 +1,40 @@
-import React from "react";
-
+import React, {useRef} from "react";
+import { getImageUrl } from "../../utils";
 import styles from "./Projects.module.css";
 
 import projects from "../../data/Projects.json";
 import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
+
+const sliderRef = useRef(null);
+
+  const scrollAmount = 250; // adjust depending on your card width
+
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    }
+  };
+
   return (
     <section className={styles.container} id="projects">
 
       <h2 className={styles.title}>Projects</h2>
 
       <div className={styles.sliderwrapper}>
-        {/* <button className={styles.navleft} id="prevBtn">‹</button> */}
         
-        <div className={styles.slidercontainer}>
+        <button className={styles.navleft} onClick={scrollLeft}>
+          <img src={getImageUrl("icons/arrow-left-circle.svg")} alt="Prev" />
+          </button>
+        
+        <div className={styles.slidercontainer} ref={sliderRef}>
           <div className={styles.cardslider}>
             {projects.map((project, id) => {
               return <ProjectCard key={id} project={project} />;
@@ -22,9 +42,15 @@ export const Projects = () => {
           </div>
         </div>
         
-        {/* <button className={styles.navright} id="nextBtn">›</button> */}
+        <button className={styles.navright} onClick={scrollRight}>
+          <img src={getImageUrl("icons/arrow-right-circle.svg")} alt="Next" />
+        </button>
+
       </div>
 
     </section>
   );
 };
+
+// id="nextBtn" 
+// id="prevBtn"
--- src/components/WorkEx/WorkEx.jsx
+++ src/components/WorkEx/WorkEx.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./WorkEx.module.css";
 import history from "../../data/WorkEx.json";
@@ -6,6 +6,17 @@ import { getImageUrl } from "../../utils";
 
 
 export const WorkEx = () => {
+
+const [modal, setModal] = useState(null);
+
+const openModal = (historyItem) => {
+    setModal(historyItem); // set selected history item
+  };
+
+const closeModal = () => {
+    setModal(null);
+  };
+  
   return (
     
     <section className={styles.container} id="workex">
@@ -15,16 +26,39 @@ export const WorkEx = () => {
           {history.map((historyItem, id) => {
             return (
               <li key={id} className={styles.historyItem}>
+
                 <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}`}</h3>
                   <h4>{`${historyItem.organisation}`}</h4>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
-                  <ul>
-                    {historyItem.experiences.map((experience, id) => {
-                      return <li className={styles.historyli} key={id}>{experience}</li>;
-                    })}
-                  </ul>
                 </div>
+
+                <button className={styles.viewDetailsBtn} onClick={() => openModal(historyItem)}>
+                    View Details
+                </button>
+
+                {/* Modal */}
+
+                {modal && (
+                  <div className={styles.modalOverlay}>
+                    <div className={styles.modalContainer} onClick={closeModal}>
+                      <div className={styles.modalItemDetails}>
+                        <h3>{`${modal.organisation}`}</h3>
+                        <ul>
+                          {modal.experiences.map((experience, id) => {
+                            return <li className={styles.modalhistoryli} key={id}>{experience}</li>;
+                          })}
+                        </ul>
+                        <div className={styles.buttonCenter}>
+                          <button className={styles.viewDetailsBtn} onClick={closeModal}>
+                            Close
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+
               </li>
             );
           })}
